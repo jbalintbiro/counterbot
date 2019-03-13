328d06099ec32b401c8d849a7655ae9fc2495d2a
Balint BIRO <j.balint.biro@gmail.com>
2019-03-13 11:17:32 +0100

    <120

--- src/main.rs
+++ src/main.rs
@@ -25,43 +25,21 @@ struct State {
 type Nick = SmallString<[u8; 9]>;
 
 impl State {
-    fn load_state_file(file: &str) -> Self {
-        let input = std::fs::read_to_string(file).expect("db file read error");
-        toml::from_str(&input).expect("db TOML parse error")
-    }
-
     fn write_state_file(&mut self, file: &str) -> std::io::Result<()> {
         let mut tmpfile = file.to_owned();
         tmpfile.push_str("._tmp");
-
         let value = toml::value::Value::try_from(&self).expect("db TOML structure error");
         std::fs::write(tmpfile.clone(), toml::to_string(&value).expect("db TOML encoding error").as_bytes())?;
         std::fs::rename(tmpfile, file)
-    }
-
-    fn increment(&mut self, u: &Nick) {
-        *self.counter.entry(u.clone()).or_insert(0) += 1;
-    }
-
-    fn top(&self) -> Vec<(Nick, u64)> {
-        let mut v: Vec<_> = self.counter.iter().map(|(k, v)| (k.clone(), *v)).collect();
-        v.sort_unstable_by(|&(_, ca), &(_, cb)| ca.cmp(&cb).reverse());
-        v
-    }
-
-    fn sum(&self) -> u64 {
-        self.counter.iter().map(|(_, c)| c).sum()
     }
 }
 
 fn absorb_message<C: Client>(client: &C, state: &mut State, settings: &Settings, chan: &str, nick: &Nick, text: &str) {
     let realnick = settings.replacements.get(nick).unwrap_or(&nick);
-
     for cw in settings.count_words.iter() {
         if text.starts_with(cw) {
-            state.increment(&realnick);
+            *state.counter.entry(realnick.clone()).or_insert(0) += 1;
             state.write_state_file(&settings.dbfile).expect("can't write db");
-            return;
         }
     }
 
@@ -72,9 +50,7 @@ fn absorb_message<C: Client>(client: &C, state: &mut State, settings: &Settings,
         _ => {}
     }
 
-    if !buf.is_empty() {
-        client.send_privmsg(chan, buf).expect("can't send")
-    }
+    if !buf.is_empty() { client.send_privmsg(chan, buf).expect("can't send") }
 }
 
 fn write_stat<W: std::fmt::Write>(buf: &mut W, state: &State, settings: &Settings, nick: &Nick) {
@@ -84,9 +60,11 @@ fn write_stat<W: std::fmt::Write>(buf: &mut W, state: &State, settings: &Setting
 }
 
 fn write_top<W: std::fmt::Write>(buf: &mut W, state: &State, settings: &Settings) {
+    let mut v: Vec<_> = state.counter.iter().map(|(k, v)| (k.clone(), *v)).collect();
+    v.sort_unstable_by(|&(_, ca), &(_, cb)| ca.cmp(&cb).reverse());
     let mut prev = std::u64::MAX;
     let mut place = 0;
-    for (nick, count) in state.top().iter().take(3) {
+    for (nick, count) in v.iter().take(3) {
         if *count != prev {
             place += 1
         };
@@ -101,30 +79,18 @@ fn write_top<W: std::fmt::Write>(buf: &mut W, state: &State, settings: &Settings
         write_count(buf, settings, *count);
     }
     write!(buf, " :: \u{2211}").unwrap();
-    let sum = state.sum();
-    write_count(buf, settings, sum);
+    write_count(buf, settings, v.iter().map(|(_, c)| c).sum());
 }
 
 fn write_count<W: std::fmt::Write>(buf: &mut W, settings: &Settings, count: u64) {
-    if let Some(color) = settings.count_color {
-        write!(buf, "\u{3}{:02}", color).unwrap();
-    }
+    if let Some(color) = settings.count_color { write!(buf, "\u{3}{:02}", color).unwrap(); }
     write!(buf, "{}{}", count, settings.count_unit).unwrap();
-    if let Some(_) = settings.count_color {
-        write!(buf, "\u{3}").unwrap();
-    }
-}
-
-fn get_nick(user: Option<String>) -> Option<Nick> {
-    user?.split('!').next().map(SmallString::from_str)
+    if let Some(_) = settings.count_color { write!(buf, "\u{3}").unwrap(); }
 }
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
-    if args.len() != 2 {
-        panic!("USAGE: {} <config>", args[0]);
-    }
-
+    if args.len() != 2 { panic!("USAGE: {} <config>", args[0]); }
     let config = std::fs::read_to_string(args[1].to_owned()).expect("config file read error");
     let settings: Settings = toml::from_str(&config).expect("config parse error");
     let irc_config = Config {
@@ -133,8 +99,8 @@ fn main() {
         channels: Some(vec![settings.channel.clone()]),
         ..Config::default()
     };
-
-    let mut state = State::load_state_file(&settings.dbfile);
+    let input = std::fs::read_to_string(&settings.dbfile).expect("db file read error");
+    let mut state = toml::from_str(&input).expect("db TOML parse error");
     let mut reactor = IrcReactor::new().unwrap();
     let client = reactor.prepare_client_and_connect(&irc_config).unwrap();
     client.identify().unwrap();
@@ -142,12 +108,11 @@ fn main() {
         let Message { prefix, command, .. } = msg;
         if let Command::PRIVMSG(ref target, ref text) = command {
             if target != &settings.channel {
-            } else if let Some(nick) = get_nick(prefix) {
+            } else if let Some(nick) = prefix.and_then(|u| u.split('!').next().map(SmallString::from_str)) {
                 absorb_message(client, &mut state, &settings, target, &nick, text);
             }
         };
         Ok(())
     });
-
     reactor.run().unwrap();
 }
