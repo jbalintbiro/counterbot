51dd912d4c927a6743f2cc8c7fcb9daa24204d31
Balint BIRO <j.balint.biro@gmail.com>
2019-03-13 15:05:18 +0100

    shorten

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
 #[macro_use]
 extern crate serde_derive;
-
 use irc::client::prelude::*;
-use smallstr::SmallString;
 
 #[derive(Debug, Deserialize)]
 struct Settings {
@@ -13,15 +11,13 @@ struct Settings {
     count_words: Vec<String>,
     count_color: Option<u8>,
     count_unit: String,
-    replacements: std::collections::HashMap<Nick, Nick>,
+    replacements: std::collections::HashMap<String, String>,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
 struct State {
-    counter: std::collections::HashMap<Nick, u64>,
+    counter: std::collections::HashMap<String, u64>,
 }
-
-type Nick = SmallString<[u8; 9]>;
 
 impl State {
     fn save_state_file(&mut self, file: &str) -> std::io::Result<()> {
@@ -30,11 +26,11 @@ impl State {
     }
 }
 
-fn absorb_message<C: Client>(client: &C, state: &mut State, settings: &Settings, chan: &str, nick: &Nick, text: &str) {
-    let realnick = settings.replacements.get(nick).unwrap_or(&nick);
+fn absorb_message<C: Client>(client: &C, state: &mut State, settings: &Settings, chan: &str, nick: &str, text: &str) {
+    let realnick: &str = settings.replacements.get(nick).map(|s| &**s).unwrap_or(&nick);
     for cw in settings.count_words.iter() {
         if text.starts_with(cw) {
-            *state.counter.entry(realnick.clone()).or_insert(0) += 1;
+            *state.counter.entry(realnick.to_owned()).or_insert(0) += 1;
             state.save_state_file(&settings.dbfile).expect("can't write db");
         }
     }
@@ -49,9 +45,9 @@ fn absorb_message<C: Client>(client: &C, state: &mut State, settings: &Settings,
     if !buf.is_empty() { client.send_privmsg(chan, buf).expect("can't send") }
 }
 
-fn write_stat<W: std::fmt::Write>(buf: &mut W, state: &State, settings: &Settings, nick: &Nick) {
+fn write_stat<W: std::fmt::Write>(buf: &mut W, state: &State, settings: &Settings, nick: &str) {
     let count = *state.counter.get(nick).unwrap_or(&0);
-    write!(buf, "{}: ", nick.as_str()).expect("can't write buffer");
+    write!(buf, "{}: ", nick).expect("can't write buffer");
     write_count(buf, settings, count);
 }
 
@@ -95,20 +91,18 @@ fn main() {
         channels: Some(vec![settings.channel.clone()]),
         ..Config::default()
     };
-    let input = std::fs::read_to_string(&settings.dbfile).expect("db file read error");
-    let mut state = toml::from_str(&input).expect("db TOML parse error");
+    let counts = std::fs::read_to_string(&settings.dbfile).expect("db file read error");
+    let mut state = toml::from_str(&counts).expect("db TOML parse error");
     let mut reactor = IrcReactor::new().unwrap();
     let client = reactor.prepare_client_and_connect(&irc_config).unwrap();
     client.identify().unwrap();
     reactor.register_client_with_handler(client, move |client, msg| {
-        let Message { prefix, command, .. } = msg;
-        if let Command::PRIVMSG(ref target, ref text) = command {
-            if target != &settings.channel {
-            } else if let Some(nick) = prefix.and_then(|u| u.split('!').next().map(SmallString::from_str)) {
-                absorb_message(client, &mut state, &settings, target, &nick, text);
+        Ok(if let Message { prefix, command: Command::PRIVMSG(target, text), .. } = msg {
+            if target != settings.channel { return Ok(()) }
+            if let Some(nick) = prefix.and_then(|u| u.split('!').next().map(|s| s.to_owned())) {
+                absorb_message(client, &mut state, &settings, &target, &nick, &text);
             }
-        };
-        Ok(())
+        })
     });
     reactor.run().unwrap();
 }
