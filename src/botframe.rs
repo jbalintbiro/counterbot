2cceeb2d7d2bae9ad81d7c70485a041c6620fbaa
Balint BIRO <j.balint.biro@gmail.com>
2019-03-19 01:49:07 +0100

    wip

--- src/botframe.rs
+++ src/botframe.rs
@@ -7,7 +7,7 @@ pub fn run_bot<F>(nick: String, server: String, chan: String, mut handler: F) ->
 where
     F: FnMut(&mut FmtWrite, &str, &str) -> crate::Result<()> + 'static,
 {
-    let mut in_buf: [u8; 4096] = [0; 4096];
+    let mut in_buf: [u8; 512] = [0; 512];
     let mut out_buf = String::new();
     let mut conn = TcpStream::connect(&server)?;
 
@@ -25,10 +25,24 @@ where
         end += count;
 
         if end > 0 {
-            let mut start = 0;
+            match in_buf.windows(2).position(|w| w == &[13, 10]) {
+                Some(line_end) => {
+                    match std::str::from_utf8(&in_buf[..line_end]) {
+                        Ok(line) => println!("IRCCMD {}", line),
+                        Err(e) => eprintln!("not utf8! {}", e),
+                    }
+
+                    let real_line_end = line_end + 2;
+                    let rest = end - real_line_end;
+                    for i in 0..std::cmp::min(rest,real_line_end) {
+                        in_buf[i] = in_buf[i+real_line_end];
+                    }
+                    end -= real_line_end;
+                }
+                None => assert!(end != 512),
+            }
 
         }
-        eprintln!("{}", end);
     }
 }
 
